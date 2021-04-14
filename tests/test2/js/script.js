563262e32755b6dab0b50fed86017e87c08fb568
Jochem Vogel <voge0031@hz.nl>
2021-04-14 16:11:30 +0200

    Update folder location tests week 2

---
+++ tests/test2/js/script.js
@@ -0,0 +1,30 @@
+const listItems = document.querySelectorAll('a');
+const manual = document.getElementById('handleiding');
+
+const welcomeMsg =
+	'Hallo Roger! Welkom op deze pagina. Graag geef ik eerst een korte toelichting. Maak gebruik van de TAB toets om door de website heen te navigeren. Mocht er uitleg nodig zijn, dan wordt deze gegeven bij de desbetreffende elementen. Met behulp van de ESCAPE toets, kun je lange stukken tekst, zoals dit, overslaan. Veel plezier met het gebruik van deze website. Gebruik de TAB toets om verder te gaan.';
+
+listItems.forEach((item) => {
+	item.addEventListener('focus', () => {
+		let utterThis = new SpeechSynthesisUtterance(item.innerText);
+		utterThis.lang = 'nl-NL';
+		window.speechSynthesis.cancel();
+		window.speechSynthesis.speak(utterThis);
+	});
+});
+
+manual.addEventListener('keydown', (e) => {
+	if (e.key === 'Enter') {
+		let utterThis = new SpeechSynthesisUtterance(welcomeMsg);
+		utterThis.lang = 'nl-NL';
+		window.speechSynthesis.speak(utterThis);
+	}
+});
+
+manual.addEventListener('click', (e) => {
+	{
+		let utterThis = new SpeechSynthesisUtterance(welcomeMsg);
+		utterThis.lang = 'nl-NL';
+		window.speechSynthesis.speak(utterThis);
+	}
+});
