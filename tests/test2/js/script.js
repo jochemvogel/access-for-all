4d6bf66abce20e7b28e9a821096a311b9049cbb6
Jochem Vogel <voge0031@hz.nl>
2021-04-14 19:45:05 +0200

    Add speech functionality to app

--- tests/test2/js/script.js
+++ tests/test2/js/script.js
@@ -1,30 +1,59 @@
-const listItems = document.querySelectorAll('a');
-const manual = document.getElementById('handleiding');
+const links = document.querySelectorAll("a, button");
+const manual = document.getElementById("handleiding");
+const voorleesBtn = document.querySelector(".voorlees-btn");
 
 const welcomeMsg =
-	'Hallo Roger! Welkom op deze website. Graag geef ik eerst een korte toelichting. Maak gebruik van de TAB toets om door de website heen te navigeren. Mocht er uitleg nodig zijn, dan wordt deze gegeven bij de desbetreffende elementen. Met behulp van de ESCAPE toets, kun je lange stukken tekst, zoals dit, overslaan. Veel plezier met het gebruik van deze website. Gebruik de TAB toets om verder te gaan.';
+    "Hallo Roger! Welkom op deze website. Hier een korte toelichting. Maak gebruik van de TAB toets om door de website heen te navigeren. Mocht er uitleg nodig zijn, dan wordt deze gegeven bij de desbetreffende elementen. Met behulp van de ESCAPE toets, kun je lange stukken tekst, zoals dit, overslaan. Met SPATIE kun je meer uitleg krijgen, zodat je precies weet wat je kunt doen. Veel plezier met het gebruik van deze website. Gebruik de TAB toets om verder te gaan.";
 
-listItems.forEach((item) => {
-	item.addEventListener('focus', () => {
-		let utterThis = new SpeechSynthesisUtterance(item.innerText);
-		utterThis.lang = 'nl-NL';
-		window.speechSynthesis.cancel();
-		window.speechSynthesis.speak(utterThis);
-	});
+links.forEach((item) => {
+    item.addEventListener("focus", () => {
+        enableSpeech(item.innerText);
+    });
+
+    item.addEventListener("keydown", (e) => {
+        if (e.code === "Space" && e.target.ariaDescribedByElements) {
+            const ariaLabel = e.target.ariaDescribedByElements[0].innerText;
+            enableSpeech(ariaLabel);
+
+            return;
+        }
+
+        if ((e.code = "Escape")) {
+            window.speechSynthesis.cancel();
+
+            return;
+        }
+    });
 });
 
-manual.addEventListener('keydown', (e) => {
-	if (e.key === 'Enter') {
-		let utterThis = new SpeechSynthesisUtterance(welcomeMsg);
-		utterThis.lang = 'nl-NL';
-		window.speechSynthesis.speak(utterThis);
-	}
+manual.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        enableSpeech(welcomeMsg, true);
+    }
 });
 
-manual.addEventListener('click', (e) => {
-	{
-		let utterThis = new SpeechSynthesisUtterance(welcomeMsg);
-		utterThis.lang = 'nl-NL';
-		window.speechSynthesis.speak(utterThis);
-	}
+manual.addEventListener("click", () => {
+    enableSpeech(welcomeMsg, true);
 });
+
+voorleesBtn.addEventListener("keydown", (e) => {
+    const article = document.querySelector("article").textContent;
+    if (e.key === "Enter") {
+        enableSpeech(article, true);
+    }
+});
+
+/**
+ *
+ * @param {string} msg - Message that will be spoken
+ * @param {boolean} isSlow - Check whether the string is an article
+ */
+function enableSpeech(msg, isSlow) {
+    let utterance = new SpeechSynthesisUtterance(msg);
+    utterance.lang = "nl-NL";
+    utterance.rate = isSlow ? 0.1 : 1;
+    utterance.volume = 0.9;
+    utterance.pitch = 0.9;
+    window.speechSynthesis.cancel();
+    window.speechSynthesis.speak(utterance);
+}
