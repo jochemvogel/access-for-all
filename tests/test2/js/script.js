e43bba9f481b2080f624324ba586899f0428c0ab
Jochem Vogel <voge0031@hz.nl>
2021-04-14 16:21:41 +0200

    Add go back button

--- tests/test2/js/script.js
+++ tests/test2/js/script.js
@@ -2,7 +2,7 @@ const listItems = document.querySelectorAll('a');
 const manual = document.getElementById('handleiding');
 
 const welcomeMsg =
-	'Hallo Roger! Welkom op deze pagina. Graag geef ik eerst een korte toelichting. Maak gebruik van de TAB toets om door de website heen te navigeren. Mocht er uitleg nodig zijn, dan wordt deze gegeven bij de desbetreffende elementen. Met behulp van de ESCAPE toets, kun je lange stukken tekst, zoals dit, overslaan. Veel plezier met het gebruik van deze website. Gebruik de TAB toets om verder te gaan.';
+	'Hallo Roger! Welkom op deze website. Graag geef ik eerst een korte toelichting. Maak gebruik van de TAB toets om door de website heen te navigeren. Mocht er uitleg nodig zijn, dan wordt deze gegeven bij de desbetreffende elementen. Met behulp van de ESCAPE toets, kun je lange stukken tekst, zoals dit, overslaan. Veel plezier met het gebruik van deze website. Gebruik de TAB toets om verder te gaan.';
 
 listItems.forEach((item) => {
 	item.addEventListener('focus', () => {
