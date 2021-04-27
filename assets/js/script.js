5cc62ceb53ec363f478f38bd0e13bb4c7b0159fb
Jochem Vogel <voge0031@hz.nl>
2021-04-27 11:28:55 +0200

    Add events & connect section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,15 @@ const links = document.querySelectorAll("a, button");
 const manual = document.getElementById("handleiding");
 const voorleesBtn = document.querySelector(".voorlees-btn");
 
+const navItems = document.querySelectorAll("nav ul li a");
+navItems.forEach((navItem) => {
+    if (navItem.href === window.location.href) {
+        if (navItem.innerHTML !== "Handleiding") {
+            navItem.classList.add("active");
+        }
+    }
+});
+
 const welcomeMsg =
     "Hallo Roger! Welkom op deze website. Hier een korte toelichting. Maak gebruik van de TAB toets om door de website heen te navigeren. Mocht er uitleg nodig zijn, dan wordt deze gegeven bij de desbetreffende elementen. Met behulp van de ESCAPE toets, kun je lange stukken tekst, zoals dit, overslaan. Met SPATIE kun je meer uitleg krijgen, zodat je precies weet wat je kunt doen. Veel plezier met het gebruik van deze website. Gebruik de TAB toets om verder te gaan.";
 
