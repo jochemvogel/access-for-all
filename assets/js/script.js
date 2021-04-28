300e558b27f584c8aefa609c39a439e864842778
Jochem Vogel <voge0031@hz.nl>
2021-04-28 17:03:15 +0200

    Fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,33 @@
-const links = document.querySelectorAll("a, button");
+const tabbableItems = document.querySelectorAll("a, button, .tabbable");
 const manual = document.getElementById("handleiding");
 const voorleesBtn = document.querySelector(".voorlees-btn");
+const articleHeadings = document.querySelectorAll(".article-list article h2");
+const articleSubHeadings = document.querySelectorAll(".subheading-article");
+
+const navItems = document.querySelectorAll("nav ul li a");
+navItems.forEach((navItem) => {
+    if (navItem.href === window.location.href) {
+        if (navItem.innerHTML !== "Handleiding") {
+            navItem.classList.add("active");
+        }
+    }
+});
 
 const welcomeMsg =
-    "Hallo Roger! Welkom op deze website. Hier een korte toelichting. Maak gebruik van de TAB toets om door de website heen te navigeren. Mocht er uitleg nodig zijn, dan wordt deze gegeven bij de desbetreffende elementen. Met behulp van de ESCAPE toets, kun je lange stukken tekst, zoals dit, overslaan. Met SPATIE kun je meer uitleg krijgen, zodat je precies weet wat je kunt doen. Veel plezier met het gebruik van deze website. Gebruik de TAB toets om verder te gaan.";
+    "Hallo Rosjee! Welkom op deze website. Hier een korte toelichting. Maak gebruik van de TAB toets om door de website heen te navigeren. Mocht er uitleg nodig zijn, dan wordt deze gegeven bij de desbetreffende elementen. Met behulp van de ESCAPE toets, kun je lange stukken tekst, zoals dit, overslaan. Met de I-toets kun je meer uitleg krijgen, zodat je precies weet wat je moet doen en welke informatie erbij hoort. Klik op de L-toets om iets voor te lezen. Veel plezier met het gebruik van deze website. Gebruik de TAB toets om verder te gaan. Ohja, klik op M om mij een mop te laten voorlezen. Dit kun je op de hele website gebruiken";
 
-links.forEach((item) => {
-    // item.addEventListener("focus", () => {
-    //     enableSpeech(item.innerText);
-    // });
+tabbableItems.forEach((item) => {
+    item.addEventListener("focus", () => {
+        enableSpeech(item.innerText);
+    });
 
     item.addEventListener("keydown", (e) => {
-        // if (e.code === "Space" && e.target.ariaDescribedByElements) {
-        //     const ariaLabel = e.target.ariaDescribedByElements[0].innerText;
-        //     enableSpeech(ariaLabel);
+        if (e.key === "i" && e.target.ariaDescribedByElements) {
+            const ariaLabel = e.target.ariaDescribedByElements[0].innerText;
+            enableSpeech(ariaLabel);
 
-        //     return;
-        // }
+            return;
+        }
 
         if ((e.code = "Escape")) {
             window.speechSynthesis.cancel();
@@ -36,12 +47,97 @@ manual.addEventListener("click", () => {
     enableSpeech(welcomeMsg, true);
 });
 
+localStorage.setItem("isPlaying", false);
+
+// Keyboard events
+document.addEventListener("keydown", (e) => {
+    // Mop
+    if (e.key === "m") {
+        const mop = getRandomMop();
+        enableSpeech(mop, true);
+    }
+
+    // U2
+    if (e.key === "p") {
+        const isPlaying = localStorage.getItem("isPlaying") === "true";
+
+        if (isPlaying) {
+            return;
+        }
+
+        const song = getRandomSong();
+        new Audio(song).play();
+        localStorage.setItem("isPlaying", true);
+    }
+});
+
+function getRandomMop() {
+    const moppenArray = [
+        "Ik heb laatst ook nog waterpolo gedaan. Was best leuk, totdat m’n paard verdronk",
+        "Een kampeerwinkel die de tent moet sluiten is nooit grappig",
+        "Waar koopt een koe zijn kleren? In de boe-tiek",
+        "Waarom is alle honing uit Belgische supermarkten gehaald? Er zat een bijsmaak aan",
+        "Hoe noem je de kipfastfoodketen uit Oekraïne? De Kiev Sie",
+        "Wat krijg je als je een bulldog en een shitzu kruist? Bullshit!",
+        "Waarom vliegen vogels altijd naar het zuiden? Omdat lopen te ver is.",
+        "Een man belt naar Easyjet om een vlucht te boeken. De vrouw vraagt: “Met hoeveel mensen vliegt u?” Waarop de man zegt: “Weet ik veel, het is toch jullie vliegtuig!",
+    ];
+
+    let mop = moppenArray[Math.floor(Math.random() * moppenArray.length)];
+
+    return mop;
+}
+
+function getRandomSong() {
+    const path = "../assets/mp3/u2/";
+    const extension = ".mp3";
+    const songArray = [
+        "i-still-havent-found-what-im-looking-for",
+        "one",
+        "where-the-streets-have-no-name",
+        "with-or-without-you",
+    ];
+
+    let song = songArray[Math.floor(Math.random() * songArray.length)];
+    song = `${path}${song}${extension}`;
+
+    return song;
+}
+
 // voorleesBtn.addEventListener("keydown", (e) => {
 //     const article = document.querySelector("article").textContent;
 //     if (e.key === "Enter") {
 //         enableSpeech(article, true);
 //     }
 // });
+
+articleHeadings.forEach((header) => {
+    header.addEventListener("keydown", (e) => {
+        if (e.key === "i") {
+            const article = e.target.parentNode;
+
+            const articleChilds = article.childNodes;
+
+            let paragraphs = [];
+            let msg;
+
+            articleChilds.forEach((child) => {
+                if (child.computedRole === "paragraph") {
+                    paragraphs.push(child.innerHTML);
+                }
+            });
+
+            if (paragraphs.length > 0) {
+                msg = paragraphs.join();
+            } else {
+                msg =
+                    "Helaas kun dit hier niet gebruiken. Probeer de tab toets om door te gaan.";
+            }
+
+            enableSpeech(msg);
+        }
+    });
+});
 
 /**
  *
@@ -50,10 +146,38 @@ manual.addEventListener("click", () => {
  */
 function enableSpeech(msg, isSlow) {
     let utterance = new SpeechSynthesisUtterance(msg);
+    const synth = window.speechSynthesis;
+    const voicesArr = synth.getVoices();
+
     utterance.lang = "nl-NL";
-    utterance.rate = 1;
+    utterance.rate = isSlow ? 0.75 : 1;
     utterance.volume = 0.9;
-    utterance.pitch = 0.9;
-    window.speechSynthesis.cancel();
-    window.speechSynthesis.speak(utterance);
+    utterance.voice = voicesArr[61];
+
+    synth.cancel();
+    synth.speak(utterance);
 }
+
+// BLOG LOGICA
+articleSubHeadings.forEach((header) => {
+    header.addEventListener("keydown", (e) => {
+        if (e.key === "i") {
+            const article = e.target.parentNode;
+
+            const articleChilds = article.childNodes;
+
+            let paragraphs = [];
+
+            articleChilds.forEach((child) => {
+                if (child.computedRole === "paragraph") {
+                    paragraphs.push(child.innerHTML);
+                }
+            });
+
+            const msg = paragraphs.join();
+
+            // enableSpeech(testMsg);
+            enableSpeech(msg);
+        }
+    });
+});
